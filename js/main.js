bbe1ed1a63ec81bde2e895b97b7a07622c1e30ff
andreasalvi99 <andrea.salvi.r@gmail.comm>
2025-12-16 15:16:58 +0100

    fixed if section e price calc

--- js/main.js
+++ js/main.js
@@ -2,29 +2,31 @@
 const kmToDo = parseInt(prompt("Quanti km devi percorrere?"));
 console.log("kmToDo:", kmToDo);
 
+// if(isNaN(kmToDo))
+
 // L'età del passeggero.
 const age = parseInt(prompt("Inserisci la tua età"));
 console.log("age:", age);
 
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const ticketPrice = parseInt(kmToDo * 0.21);
+const ticketPrice = kmToDo * 0.21;
 console.log("ticketPrice:", ticketPrice + "€");
-
-// Va applicato uno sconto del 20% per i minorenni
-const underAgeDisc = (ticketPrice * 20) / 100;
-const underAgeTick = (ticketPrice - underAgeDisc).toFixed(2);
 
 //  SE passeggero ha età < di 18 anni applica sconto
 if (age < 18) {
   alert("Hai diritto a uno sconto del 20%");
   console.log("underAgeTick:", underAgeTick + "€");
-}
 
-// Va applicato uno sconto del 40% per gli over 65.
-const overAgeDisc = (ticketPrice * 40) / 100;
-const overAgeTick = (ticketPrice - overAgeDisc).toFixed(2);
+  // Va applicato uno sconto del 20% per i minorenni
+  const underAgeDisc = (ticketPrice * 20) / 100;
+  const underAgeTick = (ticketPrice - underAgeDisc).toFixed(2);
 
-if (age > 65) {
+  //  SE passeggero ha età > di 65 anni applica sconto
+} else if (age > 65) {
   alert("Hai diritto a uno sconto del 40%");
   console.log("overAgeTick:", overAgeTick + "€");
+
+  // Va applicato uno sconto del 40% per gli over 65.
+  const overAgeDisc = (ticketPrice * 40) / 100;
+  const overAgeTick = (ticketPrice - overAgeDisc).toFixed(2);
 }
