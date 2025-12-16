af1fe589db9079b1ece3acc35c451bac28052896
andreasalvi99 <andrea.salvi.r@gmail.comm>
2025-12-16 14:07:02 +0100

    age and km prompt done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere
+const kmToDo = parseInt(prompt("Quanti km devi percorrere?"));
+console.log("kmToDo:", kmToDo);
+
 // L'età del passeggero.
+const age = parseInt(prompt("Inserisci la tua età"));
+console.log("age:", age);
+
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const ticketPrice = (kmToDo * 0.21) "€";
+console.log("ticketPrice:", ticketPrice);
